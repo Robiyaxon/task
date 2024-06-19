4d68081e821930f3f574e34388177bcdde656085
Robiyaxon <tuhtaevarobiahon@gmail.com>
2024-06-19 11:32:58 +0500

    fixed

--- src/page/anytime/Anytime.jsx
+++ src/page/anytime/Anytime.jsx
@@ -8,7 +8,7 @@ const Anytime = () => {
                 <p >Use anytime</p>
                 <h1 >Use anytime when you need</h1>
                 <h2>Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim accumsan nisi, tincidunt vel. Enim ipsum, amet quis ullamcorper eget ut.</h2>
-                <a href="">Learn more →</a>
+                <a href=".#">Learn more →</a>
             </div>
             <div className={style.img}>
                 <img src={img} alt="" />
